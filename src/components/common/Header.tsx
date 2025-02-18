fea3d1f2b692ed3378df1382b81dbb8576e7c8ed
chutiwatrumnum <chutiwatrumnum@gmail.com>
2025-02-19 00:30:16 +0700

    update

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,36 +1,17 @@
-import React, { useEffect, useState } from "react";
-import { Row, Col, Breadcrumb } from "antd";
-import "../style/header.css";
-import BELL_ICON from "../../assets/icons/bell.png";
-let width = window.innerWidth;
-type Prop = {
+import "../style/common.css";
+interface HeaderPropsType {
   title: string;
-  BreadcrumbData?: {
-    status: boolean;
-    data: any[];
-  } | null;
-};
-function Header({ title, BreadcrumbData }: Prop) {
-  return (
-    <Row className="heading">
-      <Col
-        span={22}
-        className="title"
-        style={{
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
-        {BreadcrumbData ? <Breadcrumb items={BreadcrumbData?.data} /> : title}
-      </Col>
-    </Row>
-  );
 }
-const styles = {
-  container: {
-    maxheight: 500,
-    overflow: "auto",
-  },
-} as const;
+
+const Header = ({ title }: HeaderPropsType) => {
+  return (
+    <>
+      <div>
+        <h1 className="titleHeader">{title}</h1>
+      </div>
+      <div className="titleHeaderBorder"></div>
+    </>
+  );
+};
 
 export default Header;
